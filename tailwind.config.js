8eb843e7d74ea3cded1689ea35ef9f31942ed13d
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-07-16 18:51:23 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "grey-900": "rgba(42, 65, 68, 1)",
+        "grey-500": "rgba(134, 162, 165, 1)",
+        white: "rgba(255, 255, 255, 1)",
+        "green-600": "rgba(12, 125, 105, 1)",
+        "green-200": "rgba(224, 241, 232, 1)",
+        red: "rgba(215, 60, 60, 1)",
+      },
+      fontFamily: {
+        karla: "Karla",
+        inter: "Inter",
+      },
+    },
+  },
+  plugins: [],
+};
