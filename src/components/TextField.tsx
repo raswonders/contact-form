0d6bc0440260d66b0702a28aebfc8085d4ace4bf
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-07-20 11:42:00 +0200

    style TextField

--- src/components/TextField.tsx
+++ src/components/TextField.tsx
@@ -4,9 +4,12 @@ export function TextField({ label }: { label: string }) {
   const id = useId();
 
   return (
-    <>
-      <input type="text" id={id} />
-      <label htmlFor={id}>{label}</label>
-    </>
+    <div className="flex flex-col gap-2">
+      <label className="font-karla" htmlFor={id}>
+        {label}
+        <span className="px-1 text-green-600">*</span>
+      </label>
+      <input className="border border-gray-500 py-3 px-8 rounded-lg" type="text" id={id} />
+    </div>
   );
 }
