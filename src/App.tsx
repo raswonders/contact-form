efddd7a9441c0dd3a73d87fd01a1ddc0053c124e
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-07-22 19:08:16 +0200

    style app

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import { Card } from "./components/Card";
 
 function App() {
   return (
-    <>
+    <div className="px-4 py-8 flex justify-center text-gray-900">
       <Card />
-    </>
+    </div>
   );
 }
 
