bd9c2470cd4b27f3047a34ce514e634d3bf5d239
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-07-22 18:45:59 +0200

    style message

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -4,9 +4,18 @@ export function Message() {
   const id = useId();
 
   return (
-    <div>
-      <label className="block" htmlFor={id}>Message</label>
-      <textarea name="message" id={id}></textarea>
+    <div className="space-y-2">
+      <label className="font-karla" htmlFor={id}>
+        Message
+        <span className="px-1 text-green-600">*</span>
+      </label>
+      <textarea
+        className="w-full border border-gray-500 py-3 px-6 rounded-lg resize-none"
+        name="message"
+        id={id}
+        rows={10}
+        required
+      ></textarea>
     </div>
   );
 }
