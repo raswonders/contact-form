93211b705ad8794f156d8f239e61d509906275e0
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-07-24 16:58:34 +0200

    add transitioning bg on hover for button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,10 +5,13 @@ interface ButtonProps {
 
 export function Button({ value, type = "button" }: ButtonProps) {
   return (
-    <input
-      className="border border-gray-500 rounded-lg bg-green-600 hover:bg-gradient-to-t hover:from-black/50 hover:to-black/50 text-white font-karla font-bold py-4 px-10"
-      type={type}
-      value={value}
-    />
+    <div className="relative font-karla text-white font-bold p-0 m-0 box-border">
+      <input className="relative z-5 w-full border border-gray-500 rounded-lg bg-green-600 py-4 px-8" />
+      <input
+        value={value}
+        type={type}
+        className="absolute inset-0 w-full h-full border border-transparent flex justify-center items-center rounded-lg text-white z-10 bg-black/0 hover:bg-black/50 transition-colors duration-300 ease-out"
+      />
+    </div>
   );
 }
