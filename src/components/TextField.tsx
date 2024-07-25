1630e31b19e4398e81e0a8dec687c96cad5c91e7
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-07-25 18:14:53 +0200

    style different TextField variants

--- src/components/TextField.tsx
+++ src/components/TextField.tsx
@@ -1,4 +1,4 @@
-import { useId } from "react";
+import { useId, useState } from "react";
 import { toCamelCase } from "../utils";
 import { Marker } from "./Marker";
 
@@ -9,19 +9,35 @@ interface TextFieldProps {
 
 export function TextField({ label, required }: TextFieldProps) {
   const id = useId();
+  const [isValid, setIsValid] = useState(true);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  function handleBlur(event: React.FocusEvent<HTMLInputElement>) {
+    let input = event.currentTarget;
+    if (!input?.checkValidity()) {
+      setIsValid(false);
+      setErrorMessage(input.validationMessage);
+    } else {
+      setIsValid(true);
+      setErrorMessage("");
+    }
+  }
 
   return (
-    <div className="space-y-2">
-      <label className="font-karla" htmlFor={id}>
+    <div className="space-y-2 font-karla">
+      <label htmlFor={id}>
         {label}
         <Marker required={required} />
       </label>
       <input
-        className="w-full border border-gray-500  py-3 px-6 rounded-lg"
+        className={`w-full border ${isValid ? "border-gray-500" : "border-red"} hover:border-green-600 focus:border-green-600 focus:outline-none py-3 px-6 rounded-lg`}
         name={toCamelCase(label)}
         type="text"
         id={id}
+        required={required}
+        onBlur={handleBlur}
       />
+      <div className="text-red">{errorMessage}</div>
     </div>
   );
 }
