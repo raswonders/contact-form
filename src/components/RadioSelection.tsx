28e908ba044550aa413462b7b88f696bfaab2418
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-07-28 13:47:35 +0200

    add states to radio selection together with error handling

--- src/components/RadioSelection.tsx
+++ src/components/RadioSelection.tsx
@@ -1,5 +1,8 @@
+import { useState } from "react";
 import { toCamelCase } from "../utils";
 import { Marker } from "./Marker";
+import radioChecked from "../assets/radio-checked.svg";
+import radioUnchecked from "../assets/radio-unchecked.svg";
 
 interface RadioSelectionProps {
   title: string;
@@ -12,31 +15,63 @@ export function RadioSelection({
   options,
   required,
 }: RadioSelectionProps) {
+  const [optionSelected, setOptionSelected] = useState("");
+  const [isValid, setIsValid] = useState(true);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  function handleBlur(event: React.FocusEvent<HTMLInputElement>) {
+    const input = event.currentTarget;
+
+    if (input?.checkValidity()) {
+      setIsValid(true);
+      setErrorMessage("");
+    }
+  }
+
+  function handleInvalid(event: React.InvalidEvent<HTMLInputElement>) {
+    const input = event.currentTarget;
+    setIsValid(false);
+    setErrorMessage(input.validationMessage);
+  }
+
   return (
-    <fieldset className="space-y-4">
-      <legend className="font-karla">
+    <fieldset className="space-y-2 font-karla">
+      <legend>
         {title}
         <Marker required={required} />
       </legend>
-      {options.map((option, i) => {
-        return (
-          <div
-            key={option}
-            className="flex gap-3 border border-gray-500 py-3 px-6 rounded-lg"
-          >
-            <input
-              {...(i === 0 ? { defaultChecked: true } : {})}
-              name={toCamelCase(title)}
-              type="radio"
-              value={option}
-              id={`option${i}`}
-            />
-            <label className="font-karla text-lg" htmlFor={`option${i}`}>
-              {option}
-            </label>
-          </div>
-        );
-      })}
+      <div className="space-y-4">
+        {options.map((option, i) => {
+          return (
+            <div
+              key={option}
+              className={`flex gap-3 border ${isValid ? "border-gray-500" : "border-red"} hover:border-green-600 focus:border-green-600 focus:outline-none rounded-lg`}
+            >
+              <label className="w-full flex gap-2 text-lg  py-3 px-6">
+                <img
+                  src={
+                    optionSelected === option ? radioChecked : radioUnchecked
+                  }
+                  aria-hidden
+                />
+                <input
+                  name={toCamelCase(title)}
+                  type="radio"
+                  value={option}
+                  id={`option${i}`}
+                  className="sr-only"
+                  onChange={() => setOptionSelected(option)}
+                  onInvalid={handleInvalid}
+                  onBlur={handleBlur}
+                  required={required}
+                />
+                {option}
+              </label>
+            </div>
+          );
+        })}
+      </div>
+      <div className="text-red">{errorMessage}</div>
     </fieldset>
   );
 }
