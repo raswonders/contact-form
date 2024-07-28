082806108333cc9ea3aa873089cade058576a463
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-07-28 14:15:26 +0200

    make app padding responsive to tablet size

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Card } from "./components/Card";
 
 function App() {
   return (
-    <div className="px-4 py-8 flex justify-center text-gray-900">
+    <div className="px-4 md:px-8 py-8 md:py-32 flex justify-center text-gray-900">
       <Card />
     </div>
   );
